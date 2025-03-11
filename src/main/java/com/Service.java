efb3976bd326f418f36526d7dbd2d062a8cefa81
Krithick25 <krithickroshan2504@gmail.com>
2025-03-11 16:20:55 +0530

    second commit

--- src/main/java/com/Service.java
+++ src/main/java/com/Service.java
@@ -1,5 +1,11 @@
 package com;
 
 public class Service {
-
+	public boolean validateEmail(String email) {
+		if(email.contains("@") && email.endsWith(".com")) {
+			return true;
+		} else {
+			return false;
+		}
+	}
 }
--- src/test/java/com/test/ServiceTest.java
+++ src/test/java/com/test/ServiceTest.java
@@ -1,5 +1,22 @@
 package com.test;
 
-public class ServiceTest {
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import org.junit.jupiter.api.Test;
+
+import com.Service;
+
+public class ServiceTest {
+	@Test
+	public void TestEmailOne() {
+		Service service = new Service();
+		assertTrue(service.validateEmail("krithick@gmail.com"));
+	}
+	
+	@Test
+	public void TestEmailTwo() {
+		Service service = new Service();
+		assertFalse(service.validateEmail("krithickgmail.com"));
+	}
 }
